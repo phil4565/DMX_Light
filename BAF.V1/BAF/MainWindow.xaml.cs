193c4bc60be31c1fdd8273f5264186e5720a1636
Philip Thinggaard <38595233+phil4565@users.noreply.github.com>
2018-04-24 21:54:55 +0200

    Add files via upload

---
+++ BAF.V1/BAF/MainWindow.xaml.cs
@@ -0,0 +1,473 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static BAF.Shows.Shows;
+using System.Drawing;
+
+namespace BAF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                OpenDMX.start();
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            OpenDMX.setDmxValue(174, 255);
+
+            Thread t = new Thread(DataThread);
+            t.Start();
+
+            OpenDMX.setDmxValue(1, 255);
+        }
+
+        bool BO = false;
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool2 == false)
+            {
+                ShowBool2 = true;
+                Thread S2 = new Thread(Show2);
+                S2.Start();
+                ShowBool3 = false;
+                ShowBool4 = false;
+                ShowBool5 = false;
+                ShowBool6 = false;
+                ShowBool8 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool2 = false;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool3 == false)
+            {
+                ShowBool3 = true;
+                Thread S3 = new Thread(Show3);
+                S3.Start();
+                ShowBool2 = false;
+                ShowBool4 = false;
+                ShowBool5 = false;
+                ShowBool6 = false;
+                ShowBool8 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool3 = false;
+            }
+
+        }
+
+        public static void DataThread()
+        {
+            while (true)
+            {
+                OpenDMX.writeData();
+                Thread.Sleep(50);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool1 == false)
+            {
+                ShowBool1 = true;
+                Thread S1 = new Thread(Show1);
+                S1.Start();
+                ShowBool7 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool1 = false;
+            }
+        }
+
+        private void BO_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (BO == false)
+            {
+                OpenDMX.setDmxValue(1, 0);
+                OpenDMX.setDmxValue(173, 0);
+                OpenDMX.setDmxValue(174, 0);
+                Show1_Button.IsEnabled = false;
+                Show2_Button.IsEnabled = false;
+                Show3_Button.IsEnabled = false;
+                Show4_Button.IsEnabled = false;
+                Show5_Button.IsEnabled = false;
+                Show6_Button.IsEnabled = false;
+                LED.IsEnabled = false;
+                ShowBool1 = false;
+                BO = true;
+            }
+            else
+            {
+                OpenDMX.setDmxValue(174, 255);
+                OpenDMX.setDmxValue(1, 255);
+                Show1_Button.IsEnabled = true;
+                Show2_Button.IsEnabled = true;
+                Show3_Button.IsEnabled = true;
+                Show4_Button.IsEnabled = true;
+                Show5_Button.IsEnabled = true;
+                Show6_Button.IsEnabled = true;
+                LED.IsEnabled = true;
+                BO = false;
+            }            
+        }
+
+        private void Show4_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool4 == false)
+            {
+                ShowBool4 = true;
+                Thread S4 = new Thread(Show4);
+                S4.Start();
+                ShowBool2 = false;
+                ShowBool3 = false;
+                ShowBool5 = false;
+                ShowBool6 = false;
+                ShowBool8 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool4 = false;
+            }
+        }
+
+        private void Show5_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool5 == false)
+            {
+                ShowBool5 = true;
+                Thread S5 = new Thread(Show5);
+                S5.Start();
+                ShowBool2 = false;
+                ShowBool3 = false;
+                ShowBool4 = false;
+                ShowBool6 = false;
+                ShowBool8 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool5 = false;
+            }
+        }
+
+        private void Show6_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool6 == false)
+            {
+                ShowBool6 = true;
+                Thread S6 = new Thread(Show6);
+                S6.Start();
+                ShowBool2 = false;
+                ShowBool3 = false;
+                ShowBool4= false;
+                ShowBool5 = false;
+                ShowBool8 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool6 = false;
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Show7_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool7 == false)
+            {
+                ShowBool7 = true;
+                Thread S7 = new Thread(Show7);
+                S7.Start();
+                ShowBool1 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool7 = false;
+            }
+        }
+
+        private void Show8_Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool8 == false)
+            {
+                ShowBool8 = true;
+                Thread S8 = new Thread(Show8);
+                S8.Start();
+                ShowBool2 = false;
+                ShowBool3 = false;
+                ShowBool4 = false;
+                ShowBool5 = false;
+                ShowBool6 = false;
+                ShowBool9 = false;
+            }
+            else
+            {
+                ShowBool8 = false;
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (OpenDMX.status == FT_STATUS.FT_DEVICE_NOT_FOUND)
+                    StatusBox.Text = "No Enttec USB Device Found";
+                else if (OpenDMX.status == FT_STATUS.FT_OK)
+                {
+                    StatusBox.Text = "Found DMX on USB";
+                }
+                else
+                    StatusBox.Text = "Error Opening Device";
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+                StatusBox.Text = "Error Connecting to Enttec USB Device";
+            }
+
+            if (ShowBool9 == false)
+            {
+                ShowBool9 = true;
+                Thread S9 = new Thread(Show9);
+                S9.Start();
+                ShowBool1 = false;
+                ShowBool2 = false;
+                ShowBool3 = false;
+                ShowBool4 = false;
+                ShowBool5 = false;
+                ShowBool6 = false;
+                ShowBool7 = false;
+                ShowBool8 = false;
+            }
+            else
+            {
+                ShowBool9 = false;
+            }
+        }
+
+        private void LED_Click(object sender, RoutedEventArgs e)
+        {
+            if (bool_LED == false)
+            { 
+            OpenDMX.setDmxValue(5, 170);
+                bool_LED = true;
+            }
+            else
+            {
+                OpenDMX.setDmxValue(5, 0);
+                bool_LED = false;
+            }
+        }
+
+        bool bool_LED = false;
+    }
+
+    public enum FT_STATUS
+    {
+        FT_OK = 0,
+        FT_INVALID_HANDLE,
+        FT_DEVICE_NOT_FOUND,
+        FT_DEVICE_NOT_OPENED,
+        FT_IO_ERROR,
+        FT_INSUFFICIENT_RESOURCES,
+        FT_INVALID_PARAMETER,
+        FT_INVALID_BAUD_RATE,
+        FT_DEVICE_NOT_OPENED_FOR_ERASE,
+        FT_DEVICE_NOT_OPENED_FOR_WRITE,
+        FT_FAILED_TO_WRITE_DEVICE,
+        FT_EEPROM_READ_FAILED,
+        FT_EEPROM_WRITE_FAILED,
+        FT_EEPROM_ERASE_FAILED,
+        FT_EEPROM_NOT_PRESENT,
+        FT_EEPROM_NOT_PROGRAMMED,
+        FT_INVALID_ARGS,
+        FT_OTHER_ERROR
+    };
+}
---
+++ BAF.V1/BAF/Shows/Shows.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAF.Shows
+{
+    class Shows
+    {
+        public static bool ShowBool1 = false;
+        public static bool ShowBool2 = false;
+        public static bool ShowBool3 = false;
+        public static bool ShowBool4 = false;
+        public static bool ShowBool5 = false;
+        public static bool ShowBool6 = false;
+        public static bool ShowBool7 = false;
+        public static bool ShowBool8 = false;
+        public static bool ShowBool9 = false;
+
+        public static void Show1()
+        {
+            while (ShowBool1 == true)
+            {
+                OpenDMX.setDmxValue(172, 0);
+                OpenDMX.setDmxValue(168, 255);
+                OpenDMX.setDmxValue(173, 160);
+                Thread.Sleep(3000);
+                if (ShowBool1 == false)
+                {
+                    break;
+                }
+                OpenDMX.setDmxValue(168, 0);
+                if (ShowBool1 == false)
+                {
+                    break;
+                }
+                Thread.Sleep(3000);
+            }
+        }
+
+        public static void Show2() // Color Change
+        {
+            while (ShowBool2 == true)
+            {
+                OpenDMX.setDmxValue(187, 6); 
+
+                if (ShowBool2 == false)
+                {
+                    break;
+                }
+                Thread.Sleep(1000);
+                OpenDMX.setDmxValue(187, 15);
+
+                if (ShowBool2 == false)
+                {
+                    break;
+                }
+                Thread.Sleep(1000);
+                OpenDMX.setDmxValue(187, 20);
+
+                if (ShowBool2 == false)
+                {
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Show3() //Red Color
+
+        {
+            while (ShowBool3 == true)
+            {
+                OpenDMX.setDmxValue(187, 6);
+
+                if (ShowBool3 == false)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void Show4() // Green Color
+        {
+            while (ShowBool4 == true)
+            {
+                OpenDMX.setDmxValue(187, 15);
+                if (ShowBool4 == false)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void Show5() //Blue Color
+        {
+            while (ShowBool5 == true)
+            {
+                OpenDMX.setDmxValue(187, 20);
+
+                if (ShowBool5 == false)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void Show6() //White Color
+        {
+            while (ShowBool6 == true)
+            {
+                OpenDMX.setDmxValue(187, 25);
+
+                if (ShowBool6 == false)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void Show7() // SLOW
+        {
+            while (ShowBool7 == true)
+            {
+                OpenDMX.setDmxValue(172, 255);
+                OpenDMX.setDmxValue(168, 255);
+                OpenDMX.setDmxValue(173, 255);
+                Thread.Sleep(60000);
+                if (ShowBool7 == false)
+                {
+                    break;
+                }
+                OpenDMX.setDmxValue(168, 0);
+                if (ShowBool7 == false)
+                {
+                    break;
+                }
+                Thread.Sleep(60000);
+            }
+        }
+        public static void Show8() // 3 Red
+        {
+            while (ShowBool8 == true)
+            {
+               OpenDMX.setDmxValue(175, 6);
+               OpenDMX.setDmxValue(178, 6);
+               // OpenDMX.setDmxValue(177, 6);
+                if (ShowBool8 == false)
+                {
+                    break;
+                }
+
+            }
+        }
+        public static void Show9() //Auto Program
+        {
+            while (ShowBool9 == true)
+            {
+           OpenDMX.setDmxValue(188, 255);
+
+                if (ShowBool9 == false)
+                { 
+                    break;
+                }
+            }
+            OpenDMX.setDmxValue(188, 0);
+        }
+
+        public static class OpenDMX
+        {
+            public static byte[] buffer = new byte[513];
+            public static uint handle;
+            public static bool done = false;
+            public static bool Connected = false;
+            public static int bytesWritten = 0;
+            public static FT_STATUS status;
+
+            public const byte BITS_8 = 8;
+            public const byte STOP_BITS_2 = 2;
+            public const byte PARITY_NONE = 0;
+            public const UInt16 FLOW_NONE = 0;
+            public const byte PURGE_RX = 1;
+            public const byte PURGE_TX = 2;
+
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_Open(UInt32 uiPort, ref uint ftHandle);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_Close(uint ftHandle);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_Read(uint ftHandle, IntPtr lpBuffer, UInt32 dwBytesToRead, ref UInt32 lpdwBytesReturned);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_Write(uint ftHandle, IntPtr lpBuffer, UInt32 dwBytesToRead, ref UInt32 lpdwBytesWritten);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_SetDataCharacteristics(uint ftHandle, byte uWordLength, byte uStopBits, byte uParity);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_SetFlowControl(uint ftHandle, char usFlowControl, byte uXon, byte uXoff);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_GetModemStatus(uint ftHandle, ref UInt32 lpdwModemStatus);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_Purge(uint ftHandle, UInt32 dwMask);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_ClrRts(uint ftHandle);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_SetBreakOn(uint ftHandle);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_SetBreakOff(uint ftHandle);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_GetStatus(uint ftHandle, ref UInt32 lpdwAmountInRxQueue, ref UInt32 lpdwAmountInTxQueue, ref UInt32 lpdwEventStatus);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_ResetDevice(uint ftHandle);
+            [DllImport("FTD2XX.dll")]
+            public static extern FT_STATUS FT_SetDivisor(uint ftHandle, char usDivisor);
+
+            public static void start()
+            {
+                handle = 0;
+                status = FT_Open(0, ref handle);
+                Thread thread = new Thread(new ThreadStart(writeData));
+                thread.Start();
+            }
+
+            public static void setDmxValue(int channel, byte value)
+            {
+                if (buffer != null)
+                {
+                    buffer[channel] = value;
+                }
+            }
+
+            public static void writeData()
+            {
+                try
+                {
+                    initOpenDMX();
+                    if (OpenDMX.status == FT_STATUS.FT_OK)
+                    {
+                        status = FT_SetBreakOn(handle);
+                        status = FT_SetBreakOff(handle);
+                        bytesWritten = write(handle, buffer, buffer.Length);
+
+                        Thread.Sleep(25);
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp);
+                }
+            }
+
+            public static int write(uint handle, byte[] data, int length)
+            {
+                try
+                {
+                    IntPtr ptr = Marshal.AllocHGlobal((int)length);
+                    Marshal.Copy(data, 0, ptr, (int)length);
+                    uint bytesWritten = 0;
+                    status = FT_Write(handle, ptr, (uint)length, ref bytesWritten);
+                    return (int)bytesWritten;
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp);
+                    return 0;
+                }
+            }
+
+            public static void initOpenDMX()
+            {
+                status = FT_ResetDevice(handle);
+                status = FT_SetDivisor(handle, (char)12);
+                status = FT_SetDataCharacteristics(handle, BITS_8, STOP_BITS_2, PARITY_NONE);
+                status = FT_SetFlowControl(handle, (char)FLOW_NONE, 0, 0);
+                status = FT_ClrRts(handle);
+                status = FT_Purge(handle, PURGE_TX);
+                status = FT_Purge(handle, PURGE_RX);
+            }
+        }
+    }
+}
